f2dad7fce92d347b105f0120cb11b307d7b00bf5
dawenxi.li <wadamolyf@gmail.com>
2024-11-03 15:00:59 +0800

    修改字体大小

--- config/fonts.lua
+++ config/fonts.lua
@@ -11,7 +11,7 @@ return {
       weight = 'Medium',
    }),
    font_size = font_size,
-
+   warn_about_missing_glyphs=false,
    --ref: https://wezfurlong.org/wezterm/config/lua/config/freetype_pcf_long_family_names.html#why-doesnt-wezterm-use-the-distro-freetype-or-match-its-configuration
    freetype_load_target = 'Normal', ---@type 'Normal'|'Light'|'Mono'|'HorizontalLcd'
    freetype_render_target = 'Normal', ---@type 'Normal'|'Light'|'Mono'|'HorizontalLcd'
